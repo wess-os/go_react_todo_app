2b426f149f85bd4158d8882ff229d2deca2483f7
Wesley <wes.lol25@gmail.com>
2024-08-27 16:10:23 -0300

    feat: create routes and chakra config

---
+++ client/src/App.tsx
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { Container, Flex, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+export const BASE_URL = "http://localhost:4000/api/";
+
+function App() {
+  const location = useLocation();
+
+  const showNavbar = location.pathname !== '/login' && location.pathname !== '/register';
+
+  return (
+    <Stack h="100vh">
+      {showNavbar && <Navbar />}
+      <Flex flex="1" justifyContent="center" alignItems="center">
+        <Routes>
+          <Route path="/" element={
+            <Container>
+              <TodoForm />
+              <TodoList />
+            </Container>
+          } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Flex>
+    </Stack>
+  );
+}
+
+const Main = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default Main;
