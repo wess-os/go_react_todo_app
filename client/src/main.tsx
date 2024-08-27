e903ccebe23d610acba69933c76a2fac59ac2457
Wesley <wes.lol25@gmail.com>
2024-08-27 16:11:04 -0300

    feat: chakra config

---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Main from './App.tsx';
+import theme from './chakra/theme';
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Main />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+);
